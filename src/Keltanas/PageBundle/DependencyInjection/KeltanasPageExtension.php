c4b2bcccebfa7b07983a84cc10ff1ad39b18376d
keltanas <keltanas@gmail.com>
2014-05-11 17:26:32 +0400

    fix code style

--- src/Keltanas/PageBundle/DependencyInjection/KeltanasPageExtension.php
+++ src/Keltanas/PageBundle/DependencyInjection/KeltanasPageExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class keltanasPageExtension extends Extension
+class KeltanasPageExtension extends Extension
 {
     /**
      * {@inheritDoc}
