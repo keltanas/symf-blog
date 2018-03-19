6452ff017f14c6e74f62ea6863253f012b7e3127
keltanas <keltanas@gmail.com>
2018-03-20 01:34:48 +0300

    Fixed update script and added migration

---
+++ app/DoctrineMigrations/Version20180319223143.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Class Version20180319223143
+ * @package Application\Migrations
+ */
+class Version20180319223143 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function up(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Users DROP locked, DROP expired, DROP expires_at, DROP credentials_expired, DROP credentials_expire_at, CHANGE username username VARCHAR(180) NOT NULL, CHANGE username_canonical username_canonical VARCHAR(180) NOT NULL, CHANGE email email VARCHAR(180) NOT NULL, CHANGE email_canonical email_canonical VARCHAR(180) NOT NULL, CHANGE salt salt VARCHAR(255) DEFAULT NULL, CHANGE confirmation_token confirmation_token VARCHAR(180) DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D5428AEDC05FB297 ON Users (confirmation_token)');
+    }
+
+    /**
+     * @param Schema $schema
+     * @throws \Doctrine\DBAL\Migrations\AbortMigrationException
+     */
+    public function down(Schema $schema)
+    {
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX UNIQ_D5428AEDC05FB297 ON Users');
+        $this->addSql('ALTER TABLE Users ADD locked TINYINT(1) NOT NULL, ADD expired TINYINT(1) NOT NULL, ADD expires_at DATETIME DEFAULT NULL, ADD credentials_expired TINYINT(1) NOT NULL, ADD credentials_expire_at DATETIME DEFAULT NULL, CHANGE username username VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, CHANGE username_canonical username_canonical VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, CHANGE email email VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, CHANGE email_canonical email_canonical VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, CHANGE salt salt VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci, CHANGE confirmation_token confirmation_token VARCHAR(255) DEFAULT NULL COLLATE utf8_unicode_ci');
+    }
+}
--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull
-composer update -o --no-dev --prefer-dist
+composer install -o --no-dev --prefer-dist
 rm -rf ./app/cache/prod/
-app/console doctrine:migrations:diff --env=prod --no-debug --no-interaction
+app/console doctrine:migrations:migrate --env=prod --no-debug --no-interaction
 app/console assets:install --env=prod --no-debug
 app/console assetic:dump --env=prod --no-debug
