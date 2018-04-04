01b77632e44d39b61ed8f117c1e07eef29da3bae
keltanas <keltanas@gmail.com>
2018-04-04 14:18:25 +0300

    Added social buttons to bottom

--- run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-./app/console --env=test -n doctrine:database:create
-./app/console --env=test -n doctrine:schema:create
-./app/console --env=test -n -v doctrine:fixtures:load
-./bin/behat
-./app/console --env=test -n doctrine:database:drop --force
